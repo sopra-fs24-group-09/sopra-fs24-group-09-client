596d7352105e553d14f13341d06a00be966a3393
Shaochang Tan <478710209@qq.com>
2024-03-28 14:08:29 +0100

    AudioRecorder widget basic logic added 1. record/play/save/reverse support 2. add compilerOptions to support debugger

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AppRouter from "./components/routing/routers/AppRouter";
-// import {VolumeBar} from "components/ui/VolumeBar";
+import AudioRecorder from "./components/ui/AudioRecorder";
 
 /**
  * Happy coding!
@@ -9,15 +9,11 @@ import AppRouter from "./components/routing/routers/AppRouter";
  * Updated by Marco Leder
  */
 const App = () => {
-  let volume = 0.5;
-  let setVolume = (volume) => {
-    console.log(volume);
-  };
   return (
     <div>
-      <Header height="100" />
-      {/* <VolumeBar/> */}
-      <AppRouter />
+      {/* <Header height="100" />
+      <AppRouter /> */}
+      <AudioRecorder />
     </div>
   );
 };
