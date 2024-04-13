43b1819bc8d1b5d7aa93b19e5e8dad3cab32d046
Shaochang Tan <478710209@qq.com>
2024-04-13 15:36:36 +0200

    popup className prop fix

--- src/components/ui/Popup.tsx
+++ src/components/ui/Popup.tsx
@@ -2,13 +2,14 @@ import React, { forwardRef } from "react";
 import { Button } from "./Button";
 
 type PopupProps = {
+    className?: string;
     children: React.ReactNode;
     toggleDialog: () => void;
 };
 
 const Popup = forwardRef<HTMLDivElement, PopupProps>((props, ref) => {
   return (
-    <dialog ref={ref} className="popup"
+    <dialog ref={ref} className={`popup ${props.className}`}
       onClick={
         (e) => {
           if (e.target === e.currentTarget) {
