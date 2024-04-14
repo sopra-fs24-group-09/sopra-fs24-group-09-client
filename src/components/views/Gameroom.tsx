76945bfd5b1a4439d397e562b908a250ad23bf0f
Shaochang Tan <478710209@qq.com>
2024-04-14 15:18:16 +0200

    Reorganize the structure of the game page to make the layout more structured.

--- src/components/views/Gameroom.tsx
+++ src/components/views/Gameroom.tsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useState, useRef} from "react";
-import {api, handleError} from "helpers/api";
-import {useNavigate} from "react-router-dom";
+import React, { useEffect, useState, useRef } from "react";
+import { api, handleError } from "helpers/api";
+import { useNavigate } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
-import {User} from "types";
+import { User } from "types";
 import "styles/views/Gameroom.scss";
 import "styles/views/Header.scss";
 import "styles/twemoji-amazing.css";
 import Header from "./Header";
-import {FFmpeg} from "@ffmpeg/ffmpeg";
+import { FFmpeg } from "@ffmpeg/ffmpeg";
 import AudioRecorder from "components/ui/AudioRecorder";
 import WavePlayer from "components/ui/WavePlayer";
 import { ButtonPlayer } from "components/ui/ButtonPlayer";
@@ -16,9 +16,9 @@ import { ButtonPlayer } from "components/ui/ButtonPlayer";
 const Gameroom = () => {
   const navigate = useNavigate();
   const [users, setUsers] = useState<User[]>(null);
-  const [showScore, setShowScore] = useState(false)
-  const [showReadyPopup, setShowReadyPopup] = useState(false)
-  const [gameOver, setGameOver] = useState(false)
+  const [showScore, setShowScore] = useState(false);
+  const [showReadyPopup, setShowReadyPopup] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
   // FFmpeg ref, loaded in useEffect once the page is loaded
   const ffmpegRef = useRef<FFmpeg | null>(new FFmpeg());
   /**
@@ -26,7 +26,7 @@ const Gameroom = () => {
    * need to pass an audio blob to the WavePlayer like this:
    */
   const [testAudioBlob, setTestAudioBlob] = useState<Blob | null>(null);
-  
+
   // load FFmpeg wasm module
   const loadFFmpeg = async () => {
     try {
@@ -37,9 +37,8 @@ const Gameroom = () => {
       alert("Failed to load FFmpeg: " + error);
     }
     console.log("FFmpeg loaded");
-  }
+  };
   loadFFmpeg();
-
 
   useEffect(() => {
     //fetch room info
@@ -66,7 +65,7 @@ const Gameroom = () => {
     // Attention!!: Just for testing purposes
     async function fetchAudioBlob() {
       try {
-        const audioBase64 = sessionStorage.getItem("user1_original"); 
+        const audioBase64 = sessionStorage.getItem("user1_original");
         if (!audioBase64) {
           return;
         }
@@ -78,7 +77,7 @@ const Gameroom = () => {
                 .map((c) => c.charCodeAt(0))
             ),
           ],
-          {type: "audio/webm"}
+          { type: "audio/webm" }
         );
         setTestAudioBlob(blob);
         console.log("Fetched audio blob", blob);
@@ -96,7 +95,7 @@ const Gameroom = () => {
   }, []);
 
   const togglePopup = () => {
-    setShowReadyPopup(prevState => !prevState);
+    setShowReadyPopup((prevState) => !prevState);
   };
 
   const playerReadyStatus = [
@@ -111,13 +110,13 @@ const Gameroom = () => {
         guess: 50,
         read: 20,
         details: [
-          {"word": "Lemon","role": 1, "score": 20},
-          {"word": "Apple","role": 0, "score": 30},
-          {"word": "Orange","role": 0, "score": 20}
-        ]
+          { word: "Lemon", role: 1, score: 20 },
+          { word: "Apple", role: 0, score: 30 },
+          { word: "Orange", role: 0, score: 20 },
+        ],
       },
       ready: true,
-      ifGuess: true
+      ifGuess: true,
     },
     {
       user: {
@@ -130,13 +129,13 @@ const Gameroom = () => {
         guess: 30,
         read: 0,
         details: [
-          {"word": "Lemon","role": 0, "score": 10},
-          {"word": "Apple","role": 1, "score": 0},
-          {"word": "Orange","role": 0, "score": 20}
-        ]
+          { word: "Lemon", role: 0, score: 10 },
+          { word: "Apple", role: 1, score: 0 },
+          { word: "Orange", role: 0, score: 20 },
+        ],
       },
       ready: true,
-      ifGuess: false
+      ifGuess: false,
     },
     {
       user: {
@@ -149,13 +148,13 @@ const Gameroom = () => {
         guess: 30,
         read: 20,
         details: [
-          {"word": "Lemon","role": 0, "score": 30},
-          {"word": "Apple","role": 0, "score": 0},
-          {"word": "Orange","role": 1, "score": 20}
-        ]
+          { word: "Lemon", role: 0, score: 30 },
+          { word: "Apple", role: 0, score: 0 },
+          { word: "Orange", role: 1, score: 20 },
+        ],
       },
       ready: false,
-      ifGuess: true
+      ifGuess: true,
     },
     {
       user: {
@@ -168,14 +167,14 @@ const Gameroom = () => {
         guess: 0,
         read: 60,
         details: [
-          {"word": "Lemon","role": 0, "score": 30},
-          {"word": "Apple","role": 0, "score": 10},
-          {"word": "Orange","role": 0, "score": 20}
-        ]
+          { word: "Lemon", role: 0, score: 30 },
+          { word: "Apple", role: 0, score: 10 },
+          { word: "Orange", role: 0, score: 20 },
+        ],
       },
       ready: true,
-      ifGuess: true
-    }
+      ifGuess: true,
+    },
   ];
 
   const gameInfo = {
@@ -195,20 +194,33 @@ const Gameroom = () => {
       <>
         <div className="gameroom roundstatus">
           <div className="gameroom counterdiv">
-            <i className={"twa twa-stopwatch"} style={{fontSize: "2.6rem"}}/>
+            <i className={"twa twa-stopwatch"} style={{ fontSize: "2.6rem" }} />
             <span className="gameroom counternum">50</span>
           </div>
           <div className="gameroom statusdiv">
-            <div className="gameroom speakPlayerContainer" >
+            <div className="gameroom speakPlayerContainer">
               {/*<img src={playerInfo.user.avatar} alt={playerInfo.user.name} />*/}
               <span className="gameroom playerAvatar">
-                <i className={"twa twa-" + gameInfo.currentSpeaker.avatar} style={{fontSize: "3.8rem"}}/>
-                <i className={"twa twa-studio-microphone"} style={{fontSize: "2.2rem"}}/>
+                <i
+                  className={"twa twa-" + gameInfo.currentSpeaker.avatar}
+                  style={{ fontSize: "3.8rem" }}
+                />
+                <i
+                  className={"twa twa-studio-microphone"}
+                  style={{ fontSize: "2.2rem" }}
+                />
               </span>
               <div className={"gameroom secondcolumn"}>
-                <div className="gameroom speakerName" style={{flexDirection: "row"}} >
-                  <span className="gameroom playerName">{gameInfo.currentSpeaker.name}</span>
-                  <span className="gameroom playerName">{" "+gameInfo.roundStatus+":"}</span>
+                <div
+                  className="gameroom speakerName"
+                  style={{ flexDirection: "row" }}
+                >
+                  <span className="gameroom playerName">
+                    {gameInfo.currentSpeaker.name}
+                  </span>
+                  <span className="gameroom playerName">
+                    {" " + gameInfo.roundStatus + ":"}
+                  </span>
                 </div>
                 <WavePlayer
                   className="gameroom waveplayer"
@@ -218,7 +230,11 @@ const Gameroom = () => {
             </div>
           </div>
           <div className="gameroom remindermssg">
-            <span className="gameroom remindertext">{"Try to simulate the reversed audio and reverse again to figure out the word!"}</span>
+            <span className="gameroom remindertext">
+              {
+                "Try to simulate the reversed audio and reverse again to figure out the word!"
+              }
+            </span>
             <AudioRecorder
               className="gameroom audiorecorder"
               ffmpeg={ffmpegRef.current}
@@ -238,7 +254,7 @@ const Gameroom = () => {
       currentSpeaker: PropTypes.shape({
         id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
-        avatar: PropTypes.string.isRequired
+        avatar: PropTypes.string.isRequired,
       }).isRequired,
       currentAnswer: PropTypes.string.isRequired,
       roundStatus: PropTypes.string.isRequired,
@@ -246,38 +262,65 @@ const Gameroom = () => {
     }).isRequired,
   };
 
-  const LeaderBoard = ({playerStatus}) => {
+  const LeaderBoard = ({ playerStatus }) => {
     return (
       <div className="gameroom leaderboarddiv">
         <div className="gameroom leaderboard">
           {playerStatus.map((playerInfo, index) => (
             <div className="gameroom singleScoreContainer" key={index}>
-              <span className={"gameroom ranking-"+index}>{index+1}</span>
+              <span className={"gameroom ranking-" + index}>{index + 1}</span>
               <span className="gameroom ldPlayerAvatar">
-                <i className={"twa twa-" + playerInfo.user.avatar} style={{fontSize: "2.8rem"}}/>
+                <i
+                  className={"twa twa-" + playerInfo.user.avatar}
+                  style={{ fontSize: "2.8rem" }}
+                />
               </span>
-              <span className="gameroom ldPlayerName">{playerInfo.user.name}</span>
-              <span className="gameroom scorenum" style={{gridColumn:"3"}}>{playerInfo.score.total}</span>
-              <span className="gameroom ldtitle" style={{gridColumn:"3"}} >Total</span>
-              <span className="gameroom scorenum" style={{gridColumn:"4"}}>{playerInfo.score.guess}</span>
-              <span className="gameroom ldtitle" style={{gridColumn:"4"}} >Guess</span>
-              <span className="gameroom scorenum" style={{gridColumn:"5"}}>{playerInfo.score.read}</span>
-              <span className="gameroom ldtitle" style={{gridColumn:"5"}} >Read</span>
+              <span className="gameroom ldPlayerName">
+                {playerInfo.user.name}
+              </span>
+              <span className="gameroom scorenum" style={{ gridColumn: "3" }}>
+                {playerInfo.score.total}
+              </span>
+              <span className="gameroom ldtitle" style={{ gridColumn: "3" }}>
+                Total
+              </span>
+              <span className="gameroom scorenum" style={{ gridColumn: "4" }}>
+                {playerInfo.score.guess}
+              </span>
+              <span className="gameroom ldtitle" style={{ gridColumn: "4" }}>
+                Guess
+              </span>
+              <span className="gameroom scorenum" style={{ gridColumn: "5" }}>
+                {playerInfo.score.read}
+              </span>
+              <span className="gameroom ldtitle" style={{ gridColumn: "5" }}>
+                Read
+              </span>
               {playerInfo.score.details.map((detail, detailIndex) => (
                 <React.Fragment key={detailIndex}>
-                  <span className="gameroom scorenum" style={{gridColumn: `${detailIndex + 6}`}}>{detail.score}</span>
+                  <span
+                    className="gameroom scorenum"
+                    style={{ gridColumn: `${detailIndex + 6}` }}
+                  >
+                    {detail.score}
+                  </span>
 
-                  <span className="gameroom ldtitle" style={{gridColumn:`${detailIndex + 6}`}}>{detail.word}</span>
+                  <span
+                    className="gameroom ldtitle"
+                    style={{ gridColumn: `${detailIndex + 6}` }}
+                  >
+                    {detail.word}
+                  </span>
                 </React.Fragment>
               ))}
             </div>
           ))}
         </div>
       </div>
-    )
-  }
+    );
+  };
 
-  const PlayerList = ({playerStatus}) => {
+  const PlayerList = ({ playerStatus }) => {
     return (
       <>
         <div className="gameroom roominfocontainer">
@@ -289,33 +332,45 @@ const Gameroom = () => {
             <div className="gameroom singlePlayerContainer" key={index}>
               {/*<img src={playerInfo.user.avatar} alt={playerInfo.user.name} />*/}
               <span className="gameroom playerAvatar">
-                <i className={"twa twa-" + playerInfo.user.avatar} style={{fontSize: "3.8rem"}}/>
+                <i
+                  className={"twa twa-" + playerInfo.user.avatar}
+                  style={{ fontSize: "3.8rem" }}
+                />
               </span>
               <div className={"gameroom secondcolumn"}>
-                <span
-                  className="gameroom playerName">{playerInfo.user.name}</span>
+                <span className="gameroom playerName">
+                  {playerInfo.user.name}
+                </span>
                 <span className="gameroom secondRow">
                   <span className=" gameroom scoreTitle"> Score: </span>
-                  <span className=" gameroom playerScore">{playerInfo.score.total}</span>
+                  <span className=" gameroom playerScore">
+                    {playerInfo.score.total}
+                  </span>
                   {/* {playerInfo.ifGuess ? (
                     <i className={"twa twa-speaking-head"} style={{marginTop: "1.8rem",marginLeft:"4.4rem" ,fontSize: "2.7rem"}}/>
                   ):(
                     <i className={"twa twa-studio-microphone"} style={{marginTop: "1.8rem",marginLeft:"4rem" ,fontSize: "2.8rem"}}/>
                   )} */}
                   {playerInfo.ifGuess ? (
-                    <i className={"twa twa-speaking-head"}/>
-                  ):(
-                    <i className={"twa twa-studio-microphone"}/>
+                    <i className={"twa twa-speaking-head"} />
+                  ) : (
+                    <i className={"twa twa-studio-microphone"} />
                   )}
                 </span>
               </div>
               <div className="gameroom playerStatus">
                 {playerInfo.ready ? (
-                  <i className="twa twa-check-mark-button" style={{fontSize: "1.5rem"}}/>
+                  <i
+                    className="twa twa-check-mark-button"
+                    style={{ fontSize: "1.5rem" }}
+                  />
                 ) : (
-                  <i className="twa twa-one-thirty" style={{fontSize: "1.5rem"}}/>
+                  <i
+                    className="twa twa-one-thirty"
+                    style={{ fontSize: "1.5rem" }}
+                  />
                 )}
-                <ButtonPlayer audio={testAudioBlob}/>
+                <ButtonPlayer audio={testAudioBlob} />
               </div>
             </div>
           ))}
@@ -330,7 +385,7 @@ const Gameroom = () => {
         user: PropTypes.shape({
           id: PropTypes.number.isRequired,
           name: PropTypes.string.isRequired,
-          avatar: PropTypes.string.isRequired
+          avatar: PropTypes.string.isRequired,
         }).isRequired,
         score: PropTypes.shape({
           total: PropTypes.number.isRequired,
@@ -340,14 +395,14 @@ const Gameroom = () => {
             PropTypes.shape({
               word: PropTypes.string.isRequired,
               role: PropTypes.number.isRequired,
-              score: PropTypes.number.isRequired
+              score: PropTypes.number.isRequired,
             })
-          ).isRequired
+          ).isRequired,
         }).isRequired,
         ready: PropTypes.bool.isRequired,
-        ifGuess: PropTypes.bool.isRequired
+        ifGuess: PropTypes.bool.isRequired,
       })
-    ).isRequired
+    ).isRequired,
   };
 
   PlayerList.propTypes = {
@@ -356,7 +411,7 @@ const Gameroom = () => {
         user: PropTypes.shape({
           id: PropTypes.number.isRequired,
           name: PropTypes.string.isRequired,
-          avatar: PropTypes.string.isRequired
+          avatar: PropTypes.string.isRequired,
         }).isRequired,
         score: PropTypes.shape({
           total: PropTypes.number.isRequired,
@@ -366,45 +421,47 @@ const Gameroom = () => {
             PropTypes.shape({
               word: PropTypes.string.isRequired,
               role: PropTypes.number.isRequired,
-              score: PropTypes.number.isRequired
-
+              score: PropTypes.number.isRequired,
             })
-          ).isRequired
+          ).isRequired,
         }).isRequired,
         ready: PropTypes.bool.isRequired,
-        ifGuess: PropTypes.bool.isRequired
+        ifGuess: PropTypes.bool.isRequired,
       })
-    ).isRequired
+    ).isRequired,
   };
 
   return (
     <BaseContainer className="gameroom basecontainer">
-      <Header left="28vw"/>
-      {!gameOver && !showReadyPopup && (
-        <Roundstatus gameInfo={gameInfo}/>
-      )}
-      <PlayerList playerStatus={playerReadyStatus}/>
-      {!gameOver && showReadyPopup && (
-        <div className="gameroom readypopupbg">
-          <div className="gameroom readypopupcontainer">
-            <span className="gameroom popuptitle"> Room#05</span>
-            <span className="gameroom popuptheme"> Advanced</span>
-            <span className="gameroom popuptext"> Ready to start the game?</span>
-            <div className="gameroom buttonset">
-              <div className="gameroom readybutton">Confirm</div>
-              <div className="gameroom cancelbutton">Cancel</div>
+      <Header left="28vw" />
+      <PlayerList playerStatus={playerReadyStatus} />
+      <div className="gameroom right-area">
+        {!gameOver && showReadyPopup && (
+          <div className="gameroom readypopupbg">
+            <div className="gameroom readypopupcontainer">
+              <span className="gameroom popuptitle"> Room#05</span>
+              <span className="gameroom popuptheme"> Advanced</span>
+              <span className="gameroom popuptext">
+                {" "}
+                Ready to start the game?
+              </span>
+              <div className="gameroom buttonset">
+                <div className="gameroom readybutton">Confirm</div>
+                <div className="gameroom cancelbutton">Cancel</div>
+              </div>
             </div>
           </div>
+        )}
+        {gameOver && (
+          <LeaderBoard playerStatus={playerReadyStatus}></LeaderBoard>
+        )}
+        {!gameOver && !showReadyPopup && <Roundstatus gameInfo={gameInfo} />}
+        <div className="gameroom inputarea">
+          <button onClick={togglePopup}> show </button>
+          <button onClick={() => setGameOver((prevState) => !prevState)}>
+            Over
+          </button>
         </div>
-      )}
-      {gameOver && (
-        <LeaderBoard playerStatus={playerReadyStatus}></LeaderBoard>
-      )
-
-      }
-      <div className="gameroom inputarea">
-        <button onClick={togglePopup}> show </button>
-        <button onClick={() => setGameOver(prevState => !prevState)}>Over</button>
       </div>
     </BaseContainer>
   );
