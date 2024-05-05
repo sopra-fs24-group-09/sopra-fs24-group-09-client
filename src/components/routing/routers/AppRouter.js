c7614d10738f53145366d24e001e97d822b76a61
Shaochang Tan <478710209@qq.com>
2024-05-05 12:40:24 +0200

    Implement router logic; fix #125 fix #124

--- src/components/routing/routers/AppRouter.js
+++ src/components/routing/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import {LobbyGuard} from "../routeProtectors/LobbyGuard";
 import LobbyRouter from "./LobbyRouter";
+import Lobby from "../../views/Lobby";
 import {LoginGuard} from "../routeProtectors/LoginGuard";
 import Register from "../../views/Register";
 import Login from "../../views/Login";
@@ -19,9 +20,13 @@ const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* global guard for all the routes */}
+        <Route path="/*" element={<LoginGuard />}>
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Route>
 
         <Route path="/lobby/*" element={<LobbyGuard />}>
-          <Route path="/lobby/*" element={<LobbyRouter base="/lobby"/>} />
+          <Route path="*" element={<LobbyRouter base="/lobby"/>} />
         </Route>
 
         <Route path="/login" element={<LoginGuard />}>
@@ -34,7 +39,9 @@ const AppRouter = () => {
 
         {/* no guard for gameroom page now*/}
         {/* the parameter name should match with the useParam in the Gameroom */}
-        <Route path="/rooms/:currentRoomID/:currentRoomName" element={<Gameroom/>} />
+        <Route path="/rooms/:currentRoomID/:currentRoomName" element={<LobbyGuard/>} >
+          <Route path="" element={<Gameroom />} />
+        </Route>
 
         <Route path="/" element={
           <Navigate to="/lobby" replace />
